12007e8baab1f12dc2ad9f0593c7fdab94d02b91
Christopher <chris.hem10@gmail.com>
2022-10-06 14:25:09 -0500

    Fixed migration issues and endpoints for events and users.

--- monoservice/migrations/002_create_events_table.py
+++ monoservice/migrations/002_create_events_table.py
@@ -14,20 +14,6 @@ steps = [
         """
     ],
     [
-       # "Up" SQL statement
-        """
-        CREATE TABLE user_vo (
-            href VARCHAR(50) NOT NULL UNIQUE,
-            first_name VARCHAR(50) NOT NULL UNIQUE
-        );
-
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE user_vo;
-        """
-    ],
-    [
         # "Up" SQL statement
         """
         CREATE TABLE events (
@@ -35,7 +21,7 @@ steps = [
             event_type TEXT NOT NULL check(event_type = 'shift'),
             shift_start TIMESTAMP NOT NULL,
             shift_end TIMESTAMP NOT NULL,
-            user_href VARCHAR(50) REFERENCES user_vo("href") ON DELETE CASCADE,
+            user_id INTEGER REFERENCES users("id") ON DELETE CASCADE,
             team_href VARCHAR(50) REFERENCES team_vo("href") ON DELETE CASCADE
         );
         """,
