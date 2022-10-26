c1d5875827e46c25a73e628c320620ae97bcfefd
Josh <thejoulesberg@gmail.com>
2022-10-26 17:15:44 -0500

    retry pipeline

--- teams_api/test_teams_api.py
+++ teams_api/test_teams_api.py
@@ -8,4 +8,4 @@ def test_create_team():
     created_team = TeamRepository.create(team)
     assert created_team == {'id':1, "name":"test", "type":1,"description":None, "pay_level":1} 
 
-
+# comment to update and force pipeline test
