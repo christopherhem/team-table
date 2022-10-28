fd29cd354018ad41892a2041023d9867542e000b
Louise <lwasson21@gmail.com>
2022-10-28 17:45:45 -0500

    formatting js

--- ghi/src/components/sidebar/index.js
+++ ghi/src/components/sidebar/index.js
@@ -1,36 +1,37 @@
 import React from 'react'
-import { 
-  SidebarContainer, 
-  Icon, 
-  CloseIcon, 
-  SidebarWrapper, 
-  SidebarMenu, 
-  SidebarLink, 
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
   SidebarLinkR,
-  SideBtnWrap, 
-  SidebarRoute } from './SidebarElements';
+  SideBtnWrap,
+  SidebarRoute
+} from './SidebarElements';
 
 
 
-const Sidebar = ({isOpen, toggle}) => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
     <SidebarContainer isOpen={isOpen} onClick={toggle}>
-        <Icon onClick={toggle}>
-            <CloseIcon />
-        </Icon>
-        <SidebarWrapper>
-          <SidebarMenu>
-            <SidebarLink to="about" onClick={toggle}>About Us</SidebarLink>
-            <SidebarLink to="discover" onClick={toggle}>Discover</SidebarLink>
-            <SidebarLink to="services" onClick={toggle}>Services</SidebarLink>
-            <SidebarLinkR to="signup" onClick={toggle}>Sign Up</SidebarLinkR>
-          </SidebarMenu>
-          <SideBtnWrap>
-            <SidebarRoute to="/signin">
-              Sign In
-            </SidebarRoute>
-          </SideBtnWrap>
-        </SidebarWrapper>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about" onClick={toggle}>About Us</SidebarLink>
+          <SidebarLink to="discover" onClick={toggle}>Discover</SidebarLink>
+          <SidebarLink to="services" onClick={toggle}>Services</SidebarLink>
+          <SidebarLinkR to="signup" onClick={toggle}>Sign Up</SidebarLinkR>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">
+            Sign In
+          </SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
     </SidebarContainer>
   )
 }
