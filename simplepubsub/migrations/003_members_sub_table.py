ee3864b310b0194ec0212b06a8d1dd39f6835f9b
Josh <thejoulesberg@gmail.com>
2022-10-28 18:10:51 -0500

    black reformatted files

--- simplepubsub/migrations/003_members_sub_table.py
+++ simplepubsub/migrations/003_members_sub_table.py
@@ -1,5 +1,5 @@
 steps = [
-     [
+    [
         """
         CREATE TABLE member_sub_urls(
             id SERIAL PRIMARY KEY NOT NULL,
@@ -8,14 +8,14 @@ steps = [
         """,
         """
         DROP TABLE member_sub_urls;
-        """
-     ],
-     [
+        """,
+    ],
+    [
         """
         INSERT INTO member_sub_urls (url)
         VALUES ('http://monoservice:8000/api/main/teams/members/')
         """,
         """
-        """
-     ]
+        """,
+    ],
 ]
--- simplepubsub/queries.py
+++ simplepubsub/queries.py
@@ -1,19 +1,21 @@
 import os
 from psycopg_pool import ConnectionPool
+
 pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
 from pydantic import BaseModel
 from models import Error, SubUrlOut, SubUrlIn
-from typing import  List, Optional, Union
+from typing import List, Optional, Union
+
 
 class TeamSubQueries:
     def add_sub(
         self,
-        sub:SubUrlIn,
-)->Union[Error, SubUrlOut]:
+        sub: SubUrlIn,
+    ) -> Union[Error, SubUrlOut]:
         with pool.connection() as conn:
-                with conn.cursor() as db:
-                    result = db.execute(
-                        """
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
                         INSERT INTO team_sub_urls(
                             url
                         )
@@ -22,46 +24,46 @@ class TeamSubQueries:
                         )
                         RETURNING id;
                         """,
-                        [sub.url]
-                    )
-                    id = result.fetchone()[0]
+                    [sub.url],
+                )
+                id = result.fetchone()[0]
         url = sub.dict()
-        return SubUrlOut(id = id,url = url)
-    def get_subs(self)->Union[Error, List[SubUrlOut]]:
+        return SubUrlOut(id=id, url=url)
+
+    def get_subs(self) -> Union[Error, List[SubUrlOut]]:
         try:
             with pool.connection() as conn:
-                    with conn.cursor() as db:
-                        result = db.execute(
-                            """
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
                             SELECT id, url
                             FROM team_sub_urls
                             """
-                        )
-                        return self.to_dict(result.fetchall(),result.description)
+                    )
+                    return self.to_dict(result.fetchall(), result.description)
         except:
-            return{"message":"Error in TeamSubQueries.get_subs"}
+            return {"message": "Error in TeamSubQueries.get_subs"}
 
-    def to_dict(self,rows,description):
+    def to_dict(self, rows, description):
         lst = []
         columns = [desc[0] for desc in description]
         for row in rows:
             item = {}
             for i in range(len(row)):
-                item[columns[i]]=row[i]
+                item[columns[i]] = row[i]
             lst.append(item)
         return lst
-
 
 
 class MainSubQueries:
     def add_sub(
         self,
-        sub:SubUrlIn,
-)->Union[Error, SubUrlOut]:
+        sub: SubUrlIn,
+    ) -> Union[Error, SubUrlOut]:
         with pool.connection() as conn:
-                with conn.cursor() as db:
-                    result = db.execute(
-                        """
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
                         INSERT INTO mono_sub_urls(
                             url
                         )
@@ -70,32 +72,33 @@ class MainSubQueries:
                         )
                         RETURNING id;
                         """,
-                        [sub.url]
-                    )
-                    id = result.fetchone()[0]
+                    [sub.url],
+                )
+                id = result.fetchone()[0]
         url = sub.dict()
-        return SubUrlOut(id = id,url = url)
-    def get_subs(self)->Union[Error, List[SubUrlOut]]:
+        return SubUrlOut(id=id, url=url)
+
+    def get_subs(self) -> Union[Error, List[SubUrlOut]]:
         try:
             with pool.connection() as conn:
-                    with conn.cursor() as db:
-                        result = db.execute(
-                            """
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
                             SELECT id, url
                             FROM mono_sub_urls
                             """
-                        )
-                        return self.to_dict(result.fetchall(),result.description)
+                    )
+                    return self.to_dict(result.fetchall(), result.description)
         except:
-            return{"message":"Error in MainSubQueries.get_subs"}
+            return {"message": "Error in MainSubQueries.get_subs"}
 
-    def to_dict(self,rows,description):
+    def to_dict(self, rows, description):
         lst = []
         columns = [desc[0] for desc in description]
         for row in rows:
             item = {}
             for i in range(len(row)):
-                item[columns[i]]=row[i]
+                item[columns[i]] = row[i]
             lst.append(item)
         return lst
 
@@ -103,12 +106,12 @@ class MainSubQueries:
 class MemberSubQueries:
     def add_sub(
         self,
-        sub:SubUrlIn,
-)->Union[Error, SubUrlOut]:
+        sub: SubUrlIn,
+    ) -> Union[Error, SubUrlOut]:
         with pool.connection() as conn:
-                with conn.cursor() as db:
-                    result = db.execute(
-                        """
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
                         INSERT INTO member_sub_urls(
                             url
                         )
@@ -117,12 +120,13 @@ class MemberSubQueries:
                         )
                         RETURNING id, url;
                         """,
-                        [sub.url]
-                    )
-                    id = result.fetchone()[0]
+                    [sub.url],
+                )
+                id = result.fetchone()[0]
         url = sub.dict()
-        return SubUrlOut(id = id,url = url)
-    def get_subs(self)->Union[Error, List[SubUrlOut]]:
+        return SubUrlOut(id=id, url=url)
+
+    def get_subs(self) -> Union[Error, List[SubUrlOut]]:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 result = db.execute(
@@ -131,19 +135,19 @@ class MemberSubQueries:
                     FROM member_sub_urls
                     """
                 )
-                urls = self.to_dict(result.fetchall(),result.description)
+                urls = self.to_dict(result.fetchall(), result.description)
         if type(urls) != list:
             temp = []
             temp.append(urls)
             urls = temp
         return urls
 
-    def to_dict(self,rows,description):
+    def to_dict(self, rows, description):
         lst = []
         columns = [desc[0] for desc in description]
         for row in rows:
             item = {}
             for i in range(len(row)):
-                item[columns[i]]=row[i]
+                item[columns[i]] = row[i]
             lst.append(item)
         return lst
